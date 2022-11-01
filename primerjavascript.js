3e9adb86098c3d77dc932158a211e6c425a730dd
Iribe19 <luis.jose200110@gmail.com>
2022-11-01 15:50:40 -0700

    Practica Introduccion JavaScript

---
+++ primerjavascript.js
@@ -0,0 +1,17 @@
+
+function quitarSITEC(){
+    // let pregunta = confirm("¿Seguro de quitar SITEC?");
+    if(confirm("¿Seguro de quitar SITEC?")){
+        alert("SITEC borrado ...:(");
+        let chi = document.getElementById("sitec");
+        chi.innerHTML = "Shiiiaaaale";
+    }
+}
+
+function funcionAlerta(){
+    alert("di clic");
+}
+// alert("Alerta en archivo externo javascript");
+/*
+    Varias lineas de comentarios
+*/
